8745ea49c2d17ac58d4a054598180935512361e1
Anukarsh <anukarshr@gmail.com>
2021-08-20 11:20:10 +0530

    game complete

--- script.js
+++ script.js
@@ -1,60 +1,69 @@
-score = 0
-cross = true
+score = 0;
+cross = true;
 
-document.onkeydown = function(e){
-    console.log("Key code is: ", e.keyCode)
-    if (e.keyCode==38){
-        taco = document.querySelector('.taco')
-        taco.classList.add('taco-ani')
-        setTimeout(() => {
-            taco.classList.remove('taco-ani')
-        }, 1000);
-    }
-    if (e.keyCode==39){
-        taco = document.querySelector('.taco')
-        tacoX = parseInt(window.getComputedStyle(taco, null).getPropertyValue('left'))
-        taco.style.left = tacoX + 112 + "px"
-    }
-    if (e.keyCode==37){
-        taco = document.querySelector('.taco')
-        tacoX = parseInt(window.getComputedStyle(taco, null).getPropertyValue('left'))
-        taco.style.left = (tacoX - 112) + "px"
-    }
-}
+document.onkeydown = function (e) {
+  console.log("Key code is: ", e.keyCode);
+  if (e.keyCode == 38) {
+    taco = document.querySelector(".taco");
+    taco.classList.add("taco-ani");
+    setTimeout(() => {
+      taco.classList.remove("taco-ani");
+    }, 1000);
+  }
+  if (e.keyCode == 39) {
+    taco = document.querySelector(".taco");
+    tacoX = parseInt(
+      window.getComputedStyle(taco, null).getPropertyValue("left")
+    );
+    taco.style.left = tacoX + 112 + "px";
+  }
+  if (e.keyCode == 37) {
+    taco = document.querySelector(".taco");
+    tacoX = parseInt(
+      window.getComputedStyle(taco, null).getPropertyValue("left")
+    );
+    taco.style.left = tacoX - 112 + "px";
+  }
+};
 
 setInterval(() => {
-    taco = document.querySelector('.taco')
-    gameOver = document.querySelector('.gameOver')
-    ant = document.querySelector('.ant')
+  taco = document.querySelector(".taco");
+  gameOver = document.querySelector(".gameOver");
+  ant = document.querySelector(".ant");
 
-    tx = parseInt(window.getComputedStyle(taco, null).getPropertyValue('left'))
-    ty = parseInt(window.getComputedStyle(taco, null).getPropertyValue('top'))
-    
-    ax = parseInt(window.getComputedStyle(ant, null).getPropertyValue('left'))
-    ay = parseInt(window.getComputedStyle(ant, null).getPropertyValue('top'))
+  tx = parseInt(window.getComputedStyle(taco, null).getPropertyValue("left"));
+  ty = parseInt(window.getComputedStyle(taco, null).getPropertyValue("top"));
 
-    offsetX = Math.abs(tx-ax)
-    offsetY = Math.abs(ty-ay)
+  ax = parseInt(window.getComputedStyle(ant, null).getPropertyValue("left"));
+  ay = parseInt(window.getComputedStyle(ant, null).getPropertyValue("top"));
 
-    console.log(offsetX, offsetY)
+  offsetX = Math.abs(tx - ax);
+  offsetY = Math.abs(ty - ay);
 
-    if (offsetX<100 && offsetY<85){
-        gameOver.style.visibility = 'visible';
-        ant.classList.remove('ant-ani')
-    }
-    else if(offsetX<120 && cross){
-        score+=1
-        updateScore(score)
-        cross = false
-        setTimeout(() => {
-            cross = true
-        }, 1000);
-        aniDuration = parseFloat(window.getComputedStyle(ant, null).getPropertyValue('animation-duration'))
-        newDuration = aniDuration - 0.1
-        ant.style.animationDuration = newDuration + 's'
-    }
+  console.log(offsetX, offsetY);
+
+  if (offsetX < 100 && offsetY < 85) {
+    gameOver.style.visibility = "visible";
+    ant.classList.remove("ant-ani");
+  } else if (offsetX < 120 && cross) {
+    score += 1;
+    updateScore(score);
+    cross = false;
+    setTimeout(() => {
+      cross = true;
+    }, 1000);
+    setTimeout(() => {
+      aniDuration = parseFloat(
+        window
+          .getComputedStyle(ant, null)
+          .getPropertyValue("animation-duration")
+      );
+      newDuration = aniDuration - 0.1;
+      ant.style.animationDuration = newDuration + "s";
+    }, 600);
+  }
 }, 100);
 
-function updateScore(score){
-    scoreCont.innerHTML = " Your Score: " + score
+function updateScore(score) {
+  scoreCont.innerHTML = " Your Score: " + score;
 }
